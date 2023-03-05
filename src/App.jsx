c912fc688da1b5aadf5316322ddfc8dc6f67fe26
YuliyaKalashnikova <pmalian@list.ru>
2023-03-05 13:06:21 +0700

    create: map for render Cell

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { Grid } from './components/Grid'
+import { Cell } from './components/Cell'
+
+
 export function App() {
-    return <h1>Hello world!</h1>;
+  return (
+  <>
+      <h1>tic-tac-toe</h1>
+      <Grid>
+      {Array.from(Array(9)).map((item, index) => <Cell key={index}></Cell>)}
+      </Grid>
+    </>
+  )
   }
